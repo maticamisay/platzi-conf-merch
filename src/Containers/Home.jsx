b805df1c7e11f297bdf6eb8df27729d81ecd0acd
maticamisay <maticamisay99@hotmail.com>
2021-09-15 20:28:09 -0300

    maquetacion y estilos de la lista de productos

--- src/Containers/Home.jsx
+++ src/Containers/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Products from '../Components/Products'
+import initialState from '../initialState'
 
 function Home() {
     return (
-        <h1>
-           Home 
-        </h1>
+        <>
+            <h1>
+            Home 
+            </h1>
+            <Products products={initialState.products}/>
+        </>
     )
 }
 
