71412422c303df1ff0d5330114367f942e2fba14
maticamisay <maticamisay99@hotmail.com>
2021-09-15 14:50:33 -0300

    configuracion webpack loaders para css

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../Styles/Components/App.css'
 
 function App() {
     return (
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: "development",
@@ -28,12 +29,24 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader'
+        ]
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
       filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+        filename: 'assets/[name].css',
     }),
   ],
   devServer: {
