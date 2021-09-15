c8416426bafb46b3b7a469eff2762687c8470e94
maticamisay <maticamisay99@hotmail.com>
2021-09-15 15:54:35 -0300

    instalación de eslint y prettier

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import '../Styles/Components/App.css'
+import React from 'react';
+import '../Styles/Components/App.css';
 
 function App() {
-    return (
-        <>
-            <h1>Hola mundo</h1>   
-        </>
-    )
+  return (
+    <>
+      <h1>Hola mundo</h1>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 
 import App from './Components/App';
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
